fb15edf290710e23c0141e1cbbc24486bf92b3c4
Moamen Hraden <moamen.hraden@primion.eu>
2024-09-28 08:44:39 +0200

    solve 3.1.1

--- 3-crate-engineering/1-crate-engineering/1-my-serde-app/src/main.rs
+++ 3-crate-engineering/1-crate-engineering/1-my-serde-app/src/main.rs
@@ -1,5 +1,8 @@
 //! Adapted from https://github.com/ferrous-systems/teaching-material/blob/main/assignments/serde-lifetimes.adoc
 
+
+use serde::{Deserialize, Serialize};
+
 /// pretend that we call an API and get a JSON String back
 fn fetch_data() -> String {
     String::from(
@@ -11,8 +14,8 @@ fn fetch_data() -> String {
         "#,
     )
 }
-
-#[derive(Debug)]
+ 
+#[derive(Debug, Serialize, Deserialize)]
 struct BlogPost {
     id: u32,
     title: String,
@@ -21,11 +24,18 @@ struct BlogPost {
 fn main() -> anyhow::Result<()> {
     let post: BlogPost = {
         let data = fetch_data();
-        todo!("use `serde_json` crate to parse JSON")
+        match serde_json::from_str(data.as_str()) {
+            Ok(value) => value,
+            Err(err) => panic!("{}", err),
+        }
     };
+
     println!("deserialized = {:?}", post);
 
-    let post_json: String = todo!("use `serde_json` to convert `post` to a string");
+    let post_json = match serde_json::to_string(&post) {
+        Ok(value) => value,
+        Err(err) => panic!("{}", err) 
+    };
     println!("serialized = {:?}", post_json);
 
     Ok(())
